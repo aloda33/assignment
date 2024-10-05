a3cc9dee1c1278e42cd479cbf10368e15844d8bf
aloda33 <lozanodavilaalba@gmail.com>
2024-10-04 20:13:36 -0500

    idkkkkk

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -18,10 +18,10 @@ export default function MoviesGrid() {
         movies.map(movie => (
           <div key={movie.id} className='movie-card'>
             <img src={`/images/${movie.image}`} alt={movie.title} />
-            <div className='movie-card-info'>
-              <h3 className='movie-card-title'>{movie.title}</h3>
-              <p className='movie-card-genre'>{movie.genre}</p>
-              <p className='movie-card-rating'>{movie.rating}</p>
+            <div className="movie-card-info">
+              <h3 className="movie-card-title">{movie.title}</h3>
+              <p className="movie-card-genre">{movie.genre}</p>
+              <p className="movie-card-rating">{movie.rating}</p>
             </div>
           </div>
         )
