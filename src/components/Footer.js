29044d73368dd9224b24c444e6a3f6d143e62dfe
aloda33 <lozanodavilaalba@gmail.com>
2024-10-04 19:15:09 -0500

    f

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Footer() {
+
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer className='footer'>
+            <p className='footer-text'>
+                {currentYear} Moviedux, all rights reserved.
+            </p>
+        </footer>
+    );
+}
