3b1b8f231d080403510954aa14bdcf8c5f9dbed3
aloda33 <lozanodavilaalba@gmail.com>
2024-10-04 19:12:01 -0500

    footer

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header () {
+    return (
+        <div className='header'>
+            <img className='logo' src='logo.png' alt='moviedux' />
+            <h2 className='app-subtitle'>It's time for popcorn! Find your next movie here.</h2>
+        </div>
+    );
+}
